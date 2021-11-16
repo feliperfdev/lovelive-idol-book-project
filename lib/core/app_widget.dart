fad269f4b677d25d68fa146614e3d82f7060b0fb
Felipe Ribeiro <feliper.dev@gmail.com>
2021-11-16 13:56:08 -0300

    Retirando modo `debug`

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -8,6 +8,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Love Live! School Idol Book!!',
       theme: mainTheme(),
       initialRoute: '/',
